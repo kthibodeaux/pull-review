a993e362cc9361e8deac437764941a95969b7381
Keith Thibodeaux <keith@railscoder.net>
2017-10-14 18:49:51 -0400

    Clean the code

--- lib/pull_review/comment_chain.rb
+++ lib/pull_review/comment_chain.rb
@@ -2,13 +2,13 @@ module PullReview
   class CommentChain
     def self.load(line)
       number = line.split(':').first
-      @@loaded = JSON
+      @loaded = JSON
         .parse(`curl --silent -H "Authorization: token #{ PullReview::TOKEN }" -H "Content-Type: application/json" "https://api.github.com/repos/#{ PullReview::REPO }/pulls/#{ number }/comments"`)
         .reject { |e| e.fetch('position').nil? }
     end
 
     def self.loaded
-      @@loaded
+      @loaded
     end
   end
 end
